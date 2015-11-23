1e88cc0d29c1721cfde8cc04ec90d8f8d02060de
vincenzo.ciaccio <vincenzo.ciaccio@timesofmalta.com>
2015-11-23 11:32:46 +0100

    scaffolding models add requires on composer file

---
+++ Lib/DsManager/Models/Match.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vincenzo.ciaccio
+ * Date: 23/11/2015
+ * Time: 11:08
+ */
+
+namespace App\Lib\DsManager\Models;
+
+
+class Match
+{
+
+}
---
+++ Lib/DsManager/Models/Team.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vincenzo.ciaccio
+ * Date: 23/11/2015
+ * Time: 11:07
+ */
+
+namespace App\Lib\DsManager\Models;
+
+
+class Team
+{
+
+}
