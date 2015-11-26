96686a6b6862b009575553028ee07d508c452295
vciaccio88 <vincenzo.ciaccio@gmail.com>
2015-11-26 20:31:14 +0100

    modules added match results improved

--- Lib/DsManager/Models/MatchResult.php
+++ Lib/DsManager/Models/MatchResult.php
@@ -9,35 +9,65 @@ namespace App\Lib\DsManager\Models;
  */
 class MatchResult
 {
-	private $goalHome;
-	private $goalAway;
-	private $homeTeam;
-	private $awayTeam;
+    private $goalHome;
+    private $goalAway;
+    private $homeTeam;
+    private $awayTeam;
 
-	/**
-	 * MatchResult constructor.
-	 * @param $goalHome
-	 * @param $goalAway
-	 * @param Team $home
-	 * @param Team $away
-	 */
-	public function __construct($goalHome, $goalAway, Team $home, Team $away)
-	{
-		$this->goalHome = $goalHome;
-		$this->goalAway = $goalAway;
-		//$this->homeTeam = $home;
-		//$this->awayTeam = $away;
-	}
+    /**
+     * MatchResult constructor.
+     * @param $goalHome
+     * @param $goalAway
+     * @param Team $home
+     * @param Team $away
+     */
+    public function __construct($goalHome, $goalAway, Team $home, Team $away)
+    {
+        $this->goalHome = $goalHome;
+        $this->goalAway = $goalAway;
+        $this->homeTeam = $home;
+        $this->awayTeam = $away;
+    }
 
-	/**
-	 * @return array
-	 */
-	public function toArray()
-	{
-		$result = [];
-		$result["goalHome"] = $this->goalHome;
-		$result["goalAway"] = $this->goalAway;
-		return $result;
-	}
+    public function getWinnerLoser()
+    {
+        if ($this->goalAway == $this->goalHome) {
+            return [
+                'isDraw' => true,
+                'winner' => $this->homeTeam,
+                'loser' => $this->awayTeam
+            ];
+        }
+
+        if ($this->goalHome > $this->goalAway) {
+            return [
+                'isDraw' => false,
+                'winner' => $this->homeTeam,
+                'loser' => $this->awayTeam
+            ];
+        }
+
+        if ($this->goalHome < $this->goalAway) {
+            return [
+                'isDraw' => false,
+                'winner' => $this->awayTeam,
+                'loser' => $this->homeTeam
+            ];
+        }
+
+        return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $result = [];
+        $result["goalHome"] = $this->goalHome;
+        $result["goalAway"] = $this->goalAway;
+        $result['info'] = $this->getWinnerLoser();
+        return $result;
+    }
 
 }
--- tests/ModelsTest.php
+++ tests/ModelsTest.php
@@ -8,137 +8,177 @@ class ModelsTest extends \PHPUnit_Framework_TestCase
 {
 
 
-	/**
-	 * @group Player
-	 */
-	public function testGetRandomPlayer()
-	{
-		$rndF = new \App\Lib\DsManager\Helpers\RandomFiller();
-		$player = $rndF->getPlayer();
-		var_dump($player->toArray());
-	}
+    /**
+     * @group Player
+     */
+    public function testGetRandomPlayer()
+    {
+        $rndF = new \App\Lib\DsManager\Helpers\RandomFiller();
+        $player = $rndF->getPlayer();
+        var_dump($player->toArray());
+    }
 
-	/**
-	 * @group Coach
-	 */
-	public function testGetRandomCoach()
-	{
-		$rndF = new \App\Lib\DsManager\Helpers\RandomFiller();
-		$coach = $rndF->getCoach();
-		var_dump($coach->toArray());
-	}
+    /**
+     * @group Coach
+     */
+    public function testGetRandomCoach()
+    {
+        $rndF = new \App\Lib\DsManager\Helpers\RandomFiller();
+        $coach = $rndF->getCoach();
+        var_dump($coach->toArray());
+    }
 
-	/**
-	 * @group Coaches
-	 */
-	public function testGetRandomCoaches()
-	{
-		foreach (\App\Lib\Helpers\Config::get('generic.localesSmall', 'api/') as $nat) {
-			$rndF = new \App\Lib\DsManager\Helpers\RandomFiller($nat);
-			$coach = $rndF->getCoach();
-			var_dump($coach->toArray());
-		}
-	}
+    /**
+     * @group Coaches
+     */
+    public function testGetRandomCoaches()
+    {
+        foreach (\App\Lib\Helpers\Config::get('generic.localesSmall', 'api/') as $nat) {
+            $rndF = new \App\Lib\DsManager\Helpers\RandomFiller($nat);
+            $coach = $rndF->getCoach();
+            var_dump($coach->toArray());
+        }
+    }
 
-	/**
-	 * @group Players
-	 */
-	public function testGetRandomPlayers()
-	{
-		foreach (\App\Lib\Helpers\Config::get('generic.localesSmall', 'api/') as $nat) {
-			$rndF = new \App\Lib\DsManager\Helpers\RandomFiller($nat);
-			$player = $rndF->getPlayer();
-			var_dump($player->toArray());
-		}
-	}
+    /**
+     * @group Players
+     */
+    public function testGetRandomPlayers()
+    {
+        foreach (\App\Lib\Helpers\Config::get('generic.localesSmall', 'api/') as $nat) {
+            $rndF = new \App\Lib\DsManager\Helpers\RandomFiller($nat);
+            $player = $rndF->getPlayer();
+            var_dump($player->toArray());
+        }
+    }
 
-	/**
-	 * @group Team
-	 */
-	public function testGetRandomTeam()
-	{
-		$rndF = new \App\Lib\DsManager\Helpers\RandomFiller("it_IT");
-		$team = $rndF->getTeam();
+    /**
+     * @group Team
+     */
+    public function testGetRandomTeam()
+    {
+        $rndF = new \App\Lib\DsManager\Helpers\RandomFiller("it_IT");
+        $team = $rndF->getTeam();
 
-		//var_dump($team->toArray());
-		echo "\n" . $team->name . " avg:";
-		echo $team->getAvgSkill();
+        //var_dump($team->toArray());
+        echo "\n" . $team->name . " avg:";
+        echo $team->getAvgSkill();
 
-		echo "\n Adding player:";
-		$player = $rndF->getPlayer();
-		var_dump($player->toArray());
-		$team->roster[] = $player;
-		echo "\n new avg: ";
-		echo $team->getAvgSkill();
-		echo "\n age avg: ";
-		echo $team->getAvgAge();
+        echo "\n Adding player:";
+        $player = $rndF->getPlayer();
+        var_dump($player->toArray());
+        $team->roster[] = $player;
+        echo "\n new avg: ";
+        echo $team->getAvgSkill();
+        echo "\n age avg: ";
+        echo $team->getAvgAge();
 
-	}
+    }
 
-	/**
-	 * @group Teams
-	 */
-	public function testGetRandomTeams()
-	{
-		$rndF = new \App\Lib\DsManager\Helpers\RandomFiller("es_ES");
+    /**
+     * @group Teams
+     */
+    public function testGetRandomTeams()
+    {
+        $rndF = new \App\Lib\DsManager\Helpers\RandomFiller("es_ES");
 
-		for ($i = 1; $i <= 20; $i++) {
-			echo "\n\n-------------";
-			$team = $rndF->getTeam();
-			echo "\n team: " . $team->name;
-			echo "\n skill avg: ";
-			echo $team->getAvgSkill();
-			echo "\n age avg: ";
-			echo $team->getAvgAge();
-		}
-	}
+        for ($i = 1; $i <= 20; $i++) {
+            echo "\n\n-------------";
+            $team = $rndF->getTeam();
+            echo "\n team: " . $team->name;
+            echo "\n skill avg: ";
+            echo $team->getAvgSkill();
+            echo "\n age avg: ";
+            echo $team->getAvgAge();
+        }
+    }
 
-	/**
-	 * @group Match
-	 */
-	public function testGetRandomMatch()
-	{
-		for ($i = 1; $i <= 30; $i++) {
-			$rndF = new \App\Lib\DsManager\Helpers\RandomFiller("it_IT");
-			$spanish = $rndF->getTeam();
-			$rndF = new \App\Lib\DsManager\Helpers\RandomFiller("it_IT");
-			$italian = $rndF->getTeam();
+    /**
+     * @group Match
+     */
+    public function testGetRandomMatch()
+    {
+        for ($i = 1; $i <= 30; $i++) {
+            $rndF = new \App\Lib\DsManager\Helpers\RandomFiller("it_IT");
+            $spanish = $rndF->getTeam();
+            $rndF = new \App\Lib\DsManager\Helpers\RandomFiller("it_IT");
+            $italian = $rndF->getTeam();
 
-			echo "\n" . $italian->name . "(" . $italian->getAvgSkill() . ") - " . $spanish->name . "(" . $spanish->getAvgSkill() . ") ...... ";
-			$result = (new \App\Lib\DsManager\Models\Match($italian, $spanish))->simulate()->toArray();
-			echo $result['goalHome'] . " - " . $result['goalAway'];
-			echo "\n\n";
-		}
+            echo "\n" . $italian->name . "(" . $italian->getAvgSkill() . ") - " . $spanish->name . "(" . $spanish->getAvgSkill() . ") ...... ";
+            $result = (new \App\Lib\DsManager\Models\Match($italian, $spanish))->simulate()->toArray();
+            echo $result['goalHome'] . " - " . $result['goalAway'];
+            echo "\n\n";
+        }
 
-	}
+    }
 
-	/**
-	 * @group Module
-	 */
-	public function testModule()
-	{
-		$rndF = new \App\Lib\DsManager\Helpers\RandomFiller("it_IT");
-		$team = $rndF->getTeam();
+    /**
+     * @group Matches
+     */
+    public function testGetRandomMatchesOneTeam()
+    {
+        $rndF = new \App\Lib\DsManager\Helpers\RandomFiller("it_IT");
+        $myTeam = $rndF->getTeam();
+        $win = 0;
+        $lost = 0;
+        $draw = 0;
 
-		$modules = \App\Lib\Helpers\Config::get("modules.modules", "api/");
-		$modules = array_keys($modules);
-		foreach ($modules as $mod) {
-			$module = new \App\Lib\DsManager\Models\Module($mod);
-			echo "\n------------\n\n" . $module;
-			echo "\nD: " . $module->isDefensive();
-			echo "\nB: " . $module->isBalanced();
-			echo "\nO: " . $module->isOffensive();
-			echo "\n";
-			print_r($module->getRoleNeeded());
-			echo "\napplicable? ";
-			if($module->isApplicableToTeam($team)){
-				echo " yes";
-			}else{
-				echo " no";
-			}
-		}
-		echo "\n";
-		print_r($team->playersPerRoleArray());
-	}
+        for ($i = 1; $i <= 30; $i++) {
+
+            $randomLocale = \App\Lib\Helpers\Config::get('generic.localesSmall', 'api/');
+            shuffle($randomLocale);
+            $randomLocale = $randomLocale[0];
+
+            $rndF = new \App\Lib\DsManager\Helpers\RandomFiller($randomLocale);
+            $opponent = $rndF->getTeam();
+
+            echo "\n" . $opponent->name . "(" . $opponent->getAvgSkill() . ") - " . $myTeam->name . "(" . $myTeam->getAvgSkill() . ") ...... ";
+            $result = (new \App\Lib\DsManager\Models\Match($opponent, $myTeam))->simulate()->toArray();
+            echo $result['goalHome'] . " - " . $result['goalAway'];
+            $result = $result['info'];
+            if (!$result['isDraw']) {
+                if ($result['winner']->name == $myTeam->name) {
+                    $win++;
+                } else {
+                    $lost++;
+                }
+            } else {
+                $draw++;
+            }
+            echo "\n";
+        }
+
+        echo "\nw:$win l:$lost d:$draw\n\n";
+
+    }
+
+    /**
+     * @group Module
+     */
+    public function testModule()
+    {
+        $rndF = new \App\Lib\DsManager\Helpers\RandomFiller("it_IT");
+        $team = $rndF->getTeam();
+
+        $modules = \App\Lib\Helpers\Config::get("modules.modules", "api/");
+        $modules = array_keys($modules);
+        foreach ($modules as $mod) {
+            $module = new \App\Lib\DsManager\Models\Module($mod);
+            echo "\n------------\n\n" . $module;
+            echo "\nD: " . $module->isDefensive();
+            echo "\nB: " . $module->isBalanced();
+            echo "\nO: " . $module->isOffensive();
+            echo "\n";
+            print_r($module->getRoleNeeded());
+            echo "\napplicable? ";
+            if ($module->isApplicableToTeam($team)) {
+                echo " yes";
+            } else {
+                echo " no";
+            }
+        }
+        echo "\n";
+        print_r($team->playersPerRoleArray());
+    }
 
 }
