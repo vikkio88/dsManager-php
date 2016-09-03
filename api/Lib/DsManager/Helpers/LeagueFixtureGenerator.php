7860de3ef73d20e11d1792f742774cdecf4b9228
vciaccio88 <vincenzo.ciaccio@gmail.com>
2016-09-03 22:51:10 +0100

    fix build error

--- api/Lib/DsManager/Helpers/LeagueFixtureGenerator.php
+++ api/Lib/DsManager/Helpers/LeagueFixtureGenerator.php
@@ -14,13 +14,16 @@ class LeagueFixtureGenerator
         $away = array_splice($teams, $halfSize);
         $home = $teams;
         $rounds = [];
-        for ($i = 0; $i < $numRounds ; $i++) {
-            for ($j = 0; $j < count($home); $j++) {
+        for ($i = 0; $i < $numRounds; $i++) {
+            $homeCount = count($home);
+            for ($j = 0; $j < $homeCount; $j++) {
                 $rounds[$i][$j]["home_team_id"] = $home[$j]['id'];
                 $rounds[$i][$j]["away_team_id"] = $away[$j]['id'];
             }
             if (count($home) + count($away) - 1 > 2) {
-                array_unshift($away, array_shift(array_splice($home, 1, 1)));
+                $spliced = array_splice($home, 1, 1);
+                $shifted = array_shift($spliced);
+                array_unshift($away, $shifted);
                 array_push($home, array_pop($away));
             }
         }
